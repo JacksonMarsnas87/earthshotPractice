90decf8a2012f4b05e54171939ed6554ed486956
JacksonMarsnas87 <jackson.marsnas@earthshot.ca>
2023-12-12 13:43:00 -0800

    Add buttons to switch pages

--- earthshot_designs/src/App.js
+++ earthshot_designs/src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
 
-    <div className="testDiv">
-      hello
+  const [page, setPage] = useState(1)
+
+  return (
+    <div className="App">
+      <button onClick={() => setPage(1) }>View design 1</button>
+      <button onClick={() => setPage(2) }>View design 2</button>
+
+      <div className="testDiv">
+        hello
+      </div>
+      
+      <>
+        {
+          page === 1 ?
+          <div>Page 1</div>
+
+          :
+
+          <div>Page 2</div>
+        }
+      </>
     </div>
   );
 }

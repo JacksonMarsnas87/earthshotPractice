76076a0da7dc2576c00a3e67f1ee3ef9f1cc08cb
JacksonMarsnas87 <jackson.marsnas@earthshot.ca>
2023-12-21 13:17:01 -0800

    Fix width for cards on mobile view

--- earthshot_designs/src/components/ProblemProfileCountry.js
+++ earthshot_designs/src/components/ProblemProfileCountry.js
@@ -10,7 +10,7 @@ import { useState, useEffect } from "react";
 function ProblemProfileCountry({ header, title, summary, causes, solutions, recommendations, progressBarLabels }) {
 
   const [width, setWidth] = useState(window.innerWidth);
-  const tinyScreenWidth = 500;
+  const tinyScreenWidth = 600;
 
   useEffect(() => {
     window.addEventListener("resize", () => setWidth(window.innerWidth));

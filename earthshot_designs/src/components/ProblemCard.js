f4862a3de7e4e7d44ee932f5e4162853fa6c2fc8
JacksonMarsnas87 <jackson.marsnas@earthshot.ca>
2024-01-03 10:25:55 -0800

    Improve styling on country cards

--- earthshot_designs/src/components/ProblemCard.js
+++ earthshot_designs/src/components/ProblemCard.js
@@ -4,6 +4,7 @@ import ProblemProfileCountry from "./ProblemProfileCountry";
 import CountryData from "../testdata/TestDataCountry";
 import TestGraphData from "../testdata/TestGraphData";
 import { useState, useEffect } from "react";
+import "../ProblemCard.css"
 
 // ALL data should be sent here, including the main card (header, title) AND child cards
 function ProblemCard({ header, title, problemNumber }) {
@@ -18,15 +19,15 @@ function ProblemCard({ header, title, problemNumber }) {
     }, [])
 
     return (
-        <div>
-            <div id="Header" style={{display: "flex", justifyContent: "center"}}>
+        <div className="OutterContainer">
+            <div className="ProblemHeaderCardContainer">
                 <ProblemHeaderCard header={header} title={title} summary={"Globally, 1 in 3 != safe drinking water, mostly != LDC's"} details={["Here is a fact", "Here is another fact. This one is much longer and will take up multiple lines", "Here is a third fact"]} progressBarLabels={["HIGH", "HIGH", "HIGH"]} />
             </div>
 
-            <img id="Image" src={bycountry} style={{marginTop: "40px", maxWidth: "100%"}} />
+            <img className="ByCountryImage" src={bycountry} />
 
             {/* This should be done by mapping over all of the countryData array. Since we don't have real data right now, it will be hardcoded for now */}
-            <div id="CardContainer" style={{display: "flex", flexWrap: "wrap", justifyContent: "center", marginTop: "40px", gap: "20px"}}>
+            <div className="CountryCardContainer">
                 <ProblemProfileCountry id="card1" header={header} data={countryData[0]} graphData={graphData} progressBarLabels={[{"label": "HIGH...", "colour": "#34ba5c"}, {"label": "HIGH...", "colour": "#34ba5c"}, {"label": "HIGH...", "colour": "#34ba5c"}]}/>
                 <ProblemProfileCountry id="card1" header={header} data={countryData[0]} graphData={graphData} progressBarLabels={[{"label": "HIGH...", "colour": "#34ba5c"}, {"label": "HIGH...", "colour": "#34ba5c"}, {"label": "HIGH...", "colour": "#34ba5c"}]}/>
                 <ProblemProfileCountry id="card1" header={header} data={countryData[0]} graphData={graphData} progressBarLabels={[{"label": "HIGH...", "colour": "#34ba5c"}, {"label": "HIGH...", "colour": "#34ba5c"}, {"label": "HIGH...", "colour": "#34ba5c"}]}/>

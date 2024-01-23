ebf76eb6c57b475204cb6f06bdf3dfebc87b2cbe
JacksonMarsnas87 <jackson.marsnas@earthshot.ca>
2024-01-23 10:26:22 -0800

    Add image to country card

--- earthshot_designs/src/components/CountryNav/CountryNavCard.js
+++ earthshot_designs/src/components/CountryNav/CountryNavCard.js
@@ -1,10 +1,14 @@
 import "../../styles/CountryNavCard.css"
+import bangkok from "../../images/countryImages/bangkok-thailand.jpg"
 
 function CountryNavCard({ country, continent, metrics }) {
   return (
     <div className="CountryNavCard">
-        <h1 className="NavCardCountryName">{country}</h1>
-        <p className="NavCardCountryContinent">{continent}</p>
+        <div className="CountryNavCardTextContainer">
+          <h1 className="NavCardCountryName">{country}</h1>
+          <p className="NavCardCountryContinent">{continent}</p>
+        </div>
+        <img className="NavCardCountryImage" src={bangkok} />
     </div>
   );
 }

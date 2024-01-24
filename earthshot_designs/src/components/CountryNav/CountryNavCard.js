af9f98025abfda92350a12e2bd2fc3921d7a76d5
JacksonMarsnas87 <jackson.marsnas@earthshot.ca>
2024-01-24 12:50:17 -0800

    Add bars for measuring metrics on country nav cards

--- earthshot_designs/src/components/CountryNav/CountryNavCard.js
+++ earthshot_designs/src/components/CountryNav/CountryNavCard.js
@@ -1,5 +1,6 @@
 import "../../styles/CountryNavCard.css"
 import bangkok from "../../images/countryImages/bangkok-thailand.jpg"
+import MetricBar from "./MetricBar";
 
 function CountryNavCard({ country, continent, metrics, imageName }) {
   return (
@@ -8,7 +9,26 @@ function CountryNavCard({ country, continent, metrics, imageName }) {
           <h1 className="NavCardCountryName">{country}</h1>
           <p className="NavCardCountryContinent">{continent}</p>
         </div>
+
         <img className="NavCardCountryImage" src={require(`../../images/countryImages/${imageName}.jpg`)} />
+        <div className="CountryNavCardMetricContainer">
+          <div className="CountryNavCardMetricContainerInner">
+            <p className="CountryNavCardMetric">Metric1</p>
+            <MetricBar />
+          </div>
+          <div className="CountryNavCardMetricContainerInner">
+            <p className="CountryNavCardMetric">Metric2</p>
+            <MetricBar />
+          </div>
+          <div className="CountryNavCardMetricContainerInner">
+            <p className="CountryNavCardMetric">Metric3</p>
+            <MetricBar />
+          </div>
+          <div className="CountryNavCardMetricContainerInner">
+            <p className="CountryNavCardMetric">Metric4</p>
+            <MetricBar />
+          </div>
+        </div>
     </div>
   );
 }
---
+++ earthshot_designs/src/components/CountryNav/MetricBar.js
@@ -0,0 +1,11 @@
+import "../../styles/MetricBar.css"
+
+function MetricBar() {
+  return (
+    <div className="MetricBarOutter">
+        <div className="MetricBarInner" />
+    </div>
+  );
+}
+
+export default MetricBar;

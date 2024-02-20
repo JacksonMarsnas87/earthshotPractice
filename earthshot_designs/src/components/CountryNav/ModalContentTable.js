2998e6043dbcdb116f856a062e2d8c2551de4be8
JacksonMarsnas87 <jackson.marsnas@earthshot.ca>
2024-02-20 11:21:48 -0800

    Add check to ignore ID in modal data table

--- earthshot_designs/src/components/CountryNav/ModalContentTable.js
+++ earthshot_designs/src/components/CountryNav/ModalContentTable.js
@@ -14,47 +14,51 @@ function ModalContentTable({ countryName, modalCategory, size, data }) {
                 // Object.keys(ModalData[countryName][modalCategory]).map((dataTitle) => {
                 Object.keys(data[modalCategory]).map((dataTitle) => {
                     let newData = data[modalCategory][dataTitle]
-                    return (
-                        <tr className={`ModalTableRow ${size}`}>
-                            <td className="ModalRow Name">{newData.title}</td>
-
-                            {
-                                newData.type === "text" ?
-
-                                <td className="ModalRow TextValue">{newData.value}</td>
-
-                                :
-
-                                newData.type === "bar" ?
-
-                                <td className="ModalRow BarValue">
-                                    <MetricBar isSelected={true} fill={newData.fill} content={newData.value} colour={newData.colour ? newData.colour : null} />
-                                </td>
-
-                                :
-
-                                newData.type === "menu" ?
-
-                                <td className="ModalRow Menu">
-                                    <Popup
-                                        trigger={<button className="FoodMenuButton">Hover to see common foods</button>}
-                                        position={size === "mobile" ? "top right" : "left bottom"}
-                                        on={['hover']}
-                                        closeOnDocumentClick
-                                    >
-                                        <span className="FoodMenuText">
-                                            {newData.data}
-                                        </span>
-                                    </Popup>
-                                </td>
-
-                                :
-                                
-                                // This has to be here because of the ID stored in mongoDB. Object keys are looped over, eventually reaching the ID, which we do NOT want to display
-                                <></>
-                            }
-                        </tr>
-                    )
+                    
+                    // Check if the entry is an object. The database stores IDs alongside object, which should not be displayed, hence we check the type to make sure we don't display the ID
+                    if (typeof newData === "object") {
+                        return (
+                            <tr className={`ModalTableRow ${size}`}>
+                                <td className="ModalRow Name">{newData.title}</td>
+    
+                                {
+                                    newData.type === "text" ?
+    
+                                    <td className="ModalRow TextValue">{newData.value}</td>
+    
+                                    :
+    
+                                    newData.type === "bar" ?
+    
+                                    <td className="ModalRow BarValue">
+                                        <MetricBar isSelected={true} fill={newData.fill} content={newData.value} colour={newData.colour ? newData.colour : null} />
+                                    </td>
+    
+                                    :
+    
+                                    newData.type === "menu" ?
+    
+                                    <td className="ModalRow Menu">
+                                        <Popup
+                                            trigger={<button className="FoodMenuButton">Hover to see common foods</button>}
+                                            position={size === "mobile" ? "top right" : "left bottom"}
+                                            on={['hover']}
+                                            closeOnDocumentClick
+                                        >
+                                            <span className="FoodMenuText">
+                                                {newData.data}
+                                            </span>
+                                        </Popup>
+                                    </td>
+    
+                                    :
+                                    
+                                    // This is here just in case there is an entry with an invalid type. It simply displays a blank table entry
+                                    null
+                                }
+                            </tr>
+                        )
+                    }
                 })
 
                 :
